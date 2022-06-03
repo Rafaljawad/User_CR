b5fd2f3fe10de409e55a124e8a811b13ddb67cbe
Rafal Jawad <rafal.qasim@gmail.com>
2022-06-03 04:27:46 -0400

    completed creating and reading data

---
+++ server.py
@@ -0,0 +1,33 @@
+
+from flask import Flask, render_template,request,redirect
+# import the class from friend.py
+from user import User
+app = Flask(__name__)
+#display the create user html which has the form of fisrst name , last name and email //just render html file
+@app.route('/')
+def index():
+    return render_template('create_user.html')
+# this will display all useres inside database plus the users added by form 
+@app.route('/show_user')
+def show_all():
+    # userList is the empty array that we created  usersList = []
+    #and then  Iterate over the db results and append the data that came from database to it plus the data that will come from form
+    usersList = User.show_all_users()
+    return render_template("read_all_user.html",all_users=usersList)
+
+@app.route('/create_user', methods=["POST"])
+def create_user():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "eml" : request.form["eml"]
+    }
+    # We pass the data dictionary into the save method from the User class.
+    User.save(data)
+    # direct the result to the show user route which has read all user html template.
+    return redirect('/show_user')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ user.py
@@ -0,0 +1,26 @@
+# import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
+# model the class after the user table from our database
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+#this class method responsible for retreiving all the users from the database
+    @classmethod
+    def show_all_users(cls):
+        query="SELECT * FROM users;"
+        results = connectToMySQL('users').query_db(query)
+        usersList = []
+# Iterate over the db results and create instances of users with cls.
+        for user in results:
+            usersList.append( cls(user))
+        return usersList
+#this class method will add new users to the data base
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO users ( first_name , last_name , email ) VALUES ( %(fname)s , %(lname)s , %(eml)s );"
+# data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('users').query_db( query, data )
+
